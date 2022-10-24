2cff42d8985885836662005293e6ed20026b1b1d
Dimas Aryaputra <dimasarya5667@gmail.com>
2022-10-24 10:55:28 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+  echo "Hello World! <br>";
+
+  $nama = "Dimas";
+  $umur = "20";
+
+  echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+  
+  $namaKakak = "Iin";
+  $umurKakak = 21;
+
+  echo "Nama kakak saya <strong>$namaKakak</strong>, kakak saya berusia $umurKakak tahun.<br>";
+
+  $selisihumur = $umurKakak - $umur;
+  echo "Selisih usia saya dan kakak saya adalah $selisihumur tahun"
+  ?>
