45e2b1ee5837c2119b9502a12b34545bc320d456
Dimas Aryaputra <dimasarya5667@gmail.com>
2022-11-26 16:24:14 +0700

    tambah data mahasiswa php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $con = mysqli_connect("localhost","root","","fakultas");
 // Cek konekksi dengan mySQL
 if(mysqli_connect_errno()){
     echo "Koneksi gagal ". mysqli_connect_error();
+    Keluar();
 }else{
     echo "Koneksi berhasil";
 }
@@ -13,9 +14,8 @@ if(mysqli_connect_errno()){
 $query = "SELECT * FROM mahasiswa";
 
 // tampilkan data dengan menjalankan sql query
-$result = mysqli_query($con,$query);
 $mahasiswa = [];
-if ($result){
+if ($result = mysqli_query($con,$query)){
     // tampilkan data satu per satu
     while($row = mysqli_fetch_assoc($result)){
         $mahasiswa[] = $row;
@@ -38,15 +38,27 @@ mysqli_close($con);
 </head>
 <body>
     <h1>Data Mahasiswa</h1>
-    <table border="1" style="witdh: 100%;">
+    <a href="insert.php">Tambah Data</a>
+    <table border="1" style="width: 100%;">
         <tr>
             <th>NIM</th>
             <th>Nama</th>
+            <th>Tempat Lahir</th>
+            <th>Tanggal Lahir</th>
+            <th>Alamat</th>
+            <th>Action</th>
         </tr>
-        <?php foreach($mahasiswa as $value): ?>
+        <?php foreach($mahasiswa as $row): ?>
         <tr>
-            <td><?php echo $value ["nim"]; ?></td>
-            <td><?php echo $value ["nama"]; ?></td>
+            <td><?php echo $row ['nim']; ?></td>
+            <td><?php echo $row ['nama']; ?></td>
+            <td><?php echo $row ['tempat_lahir']; ?></td>
+            <td><?php echo $row ['tanggal_lahir']; ?></td>
+            <td><?php echo $row ['alamat']; ?></td>
+            <td>
+                <a href="update.php?id=<?= $row['id'] ?>" >Edit</a> | 
+                <a href="delete.php?id=<?= $row['id'] ?>" >Delete</a>
+            </td>
         </tr>
         <?php endforeach; ?>
     </table>
--- insert.php
+++ insert.php
@@ -2,6 +2,7 @@
 
 // tangkap data dari form submit
 if (isset($_POST["submit"])){
+    // var_dump($_POST)
     $nim = $_POST['nim'];
     $nama = $_POST['nama'];
     $id_jurusan = $_POST['id_jurusan'];
@@ -36,15 +37,6 @@ mysqli_close($con);
 
 }
 
-
-
-
-
-
-
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -62,7 +54,7 @@ mysqli_close($con);
         ID Jurusan: <input type="number" name="id_jurusan"><br>
         Jenis Kelamin: <input type="text" name="gender"><br>
         Tempat Lahir: <input type="text" name="tpt_lahir"><br>
-        Tanggal Lahir (yyyy-mm-dd): <input type="number" name="tgl_lahir"><br>
+        Tanggal Lahir (yyyy-mm-dd): <input type="text" name="tgl_lahir"><br>
         Alamat: <input type="text" name="alamat"><br>
         <button type="submit" name="submit">Tambah Data</button>
     </form>
